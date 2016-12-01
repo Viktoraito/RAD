98ce697f8df961d95d7d549369d50149363843f3
M.Kofman <kkof@rambler.ru>
2016-12-01 12:48:13 -0800

    playing with glut for robot visualisation

--- server.c
+++ server.c
@@ -1,6 +1,63 @@
-#include<stdio.h>
+#include <GL/gl.h>
 
-int main()
-{
-return 0;
+#include <GL/glut.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/*dx, dy - global parameters, delta of robot's coordinates. transfered from coordinates from client xml-message*/
+float dx=0, dy=0;
+
+void Draw() {
+	float x=-0.5, y=-0.5;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glBegin(GL_QUADS);
+		glVertex3f(0.05-dx,-0.05,0.0);
+		glVertex3f(0.05-dx,0.05,0.0);
+		glVertex3f(-0.05-dx,0.05,0.0);
+		glVertex3f(-0.05-dx,-0.05,0.0);
+	glEnd();
+/*	switch (c) {
+	case 'a':
+		glTranslatef(-0.05,0,0);
+		c='\0';
+		break;
+	case 'w':
+		glTranslatef(0,0.05,0);
+		c='\0';
+		break;
+	case 's':
+		glTranslatef(0,-0.05,0);
+		c='\0';
+		break;
+	case 'd':
+		glTranslatef(0.05,0,0);
+		c='\0';
+		break;
+	default:
+		break;
+	}*/
+	glBegin(GL_QUADS);
+		glVertex3f(0.05+x+dx,-0.05+y,0.0);
+		glVertex3f(0.05+x+dx,0.05+y,0.0);
+		glVertex3f(-0.05+x+dx,0.05+y,0.0);
+		glVertex3f(-0.05+x+dx,-0.05+y,0.0);
+	glEnd();
+	glutSwapBuffers();
+}
+
+void timer (int t) {
+   dx+=0.01;
+   glutDisplayFunc( Draw );
+   glutPostRedisplay();
+   glutTimerFunc(100, timer, 0); 	
+}
+
+int main( int argc, char *argv[] ) {
+   glutInit( &argc, argv );
+   glutInitDisplayMode( GLUT_DEPTH| GLUT_DOUBLE | GLUT_RGBA);
+   glutInitWindowSize( 500, 500 );
+   glutInitWindowPosition( 100, 100 );
+   glutCreateWindow( "RAD DEMO" );
+   glutTimerFunc(1, timer, 0);
+   glutMainLoop();
 }
