03237bfef852064db7d90f20bd06c9b9205f00ff
Viktoraito <kkof@rambler.ru>
2016-12-18 05:40:39 -0800

    Start work with sockets

--- server.c
+++ server.c
@@ -3,6 +3,14 @@
 #include <GL/glut.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <sys/queue.h>
+
+#define DEFPORT 7777
+#define QUEUE 3
 
 /*dx, dy - global parameters, delta of robot's coordinates. transfered from coordinates from client xml-message*/
 float dx=0, dy=0;
@@ -52,12 +60,26 @@ void timer (int t) {
    glutTimerFunc(100, timer, 0); 	
 }
 
-int main( int argc, char *argv[] ) {
-   glutInit( &argc, argv );
+int main( int argc, char **argv) {
+  unsigned port=DEFPORT;
+  int s_sockfd;
+  struct sockaddr_in s_address;
+  s_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  s_address.sin_family = AF_INET;
+  s_address.sin_addr.s_addr = htonl(INADDR_ANY);
+  s_address.sin_port = htons(port);
+
+  if(bind(s_sockfd, (struct sockaddr *)&s_address, sizeof(s_address))) {
+    printf("Can't bind socket\n");
+    return EXIT_FAILURE;
+  }
+  else printf("SUCCESS\n");
+
+/*   glutInit( &argc, argv );
    glutInitDisplayMode( GLUT_DEPTH| GLUT_DOUBLE | GLUT_RGBA);
    glutInitWindowSize( 500, 500 );
    glutInitWindowPosition( 100, 100 );
    glutCreateWindow( "RAD DEMO" );
    glutTimerFunc(1, timer, 0);
-   glutMainLoop();
+   glutMainLoop();*/
 }
