6e53baceeabcef96d57ce924e32ed7f94365199e
Viktoraito <kkof@rambler.ru>
2016-12-18 06:18:52 -0800

    Now, client can connect with server

---
+++ common.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <libxml/encoding.h>
+#include <libxml/xmlwriter.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <sys/queue.h>
+
+#define DEFPORT 7777
